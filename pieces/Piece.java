e778a6e26abb36b937cc7b6891fd25b07d031d9b
syall <s.yuan.all@gmail.com>
2019-12-21 03:06:18 -0500

    Finish pawn, king, knight

--- pieces/Piece.java
+++ pieces/Piece.java
@@ -63,7 +63,6 @@ public abstract class Piece {
     public BiFunction<Point, Piece[][], List<Point>> diagonals = (p, board) -> {
         ArrayList<Point> moves = new ArrayList<Point>();
         Piece piece = board[p.row][p.col];
-        // Upper Left
         for (int i = 1; p.row - i >= 0 && p.col - i >= 0; i++) {
             if (board[p.row - i][p.col - i].color == piece.color)
                 break;
@@ -71,7 +70,6 @@ public abstract class Piece {
             if (board[p.row - i][p.col - i].color != 'e')
                 break;
         }
-        // Upper Right
         for (int i = 1; p.row - i >= 0 && p.col + i < 8; i++) {
             if (board[p.row - i][p.col + i].color == piece.color)
                 break;
@@ -79,7 +77,6 @@ public abstract class Piece {
             if (board[p.row - i][p.col + i].color != 'e')
                 break;
         }
-        // Lower Left
         for (int i = 1; p.row + i < 8 && p.col - i >= 0; i++) {
             if (board[p.row + i][p.col - i].color == piece.color)
                 break;
@@ -87,7 +84,6 @@ public abstract class Piece {
             if (board[p.row + i][p.col - i].color != 'e')
                 break;
         }
-        // Lower Right
         for (int i = 1; p.row + i < 8 && p.col + i < 8; i++) {
             if (board[p.row + i][p.col + i].color == piece.color)
                 break;
@@ -101,18 +97,61 @@ public abstract class Piece {
     public BiFunction<Point, Piece[][], List<Point>> pawn = (p, board) -> {
         ArrayList<Point> moves = new ArrayList<Point>();
         Piece piece = board[p.row][p.col];
+        int d = piece.color == 'b' ? -1 : 1;
+        char opp = piece.color == 'b' ? 'w' : 'b';
+        if (p.row + d >= 0 && p.row + d < 8 && board[p.row + d][p.col].color == 'e') {
+            moves.add(new Point(p.row + d, p.col));
+            if (p.row + 2 * d >= 0 && p.row + 2 * d < 8 && board[p.row + 2 * d][p.col].color == 'e' && !piece.moved)
+                moves.add(new Point(p.row + 2 * d, p.col));
+        }
+        if (p.row + d >= 0 && p.row + d < 8 && p.col - 1 >= 0 && board[p.row + d][p.col - 1].color == opp)
+            moves.add(new Point(p.row + d, p.col - 1));
+        if (p.row + d >= 0 && p.row + d < 8 && p.col + 1 < 8 && board[p.row + d][p.col + 1].color == opp)
+            moves.add(new Point(p.row + d, p.col + 1));
         return moves;
     };
 
     public BiFunction<Point, Piece[][], List<Point>> king = (p, board) -> {
         ArrayList<Point> moves = new ArrayList<Point>();
         Piece piece = board[p.row][p.col];
+        if (p.row - 1 >= 0 && p.col - 1 >= 0 && board[p.row - 1][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row - 1, p.col - 1));
+        if (p.row - 1 >= 0 && p.col + 1 < 8 && board[p.row - 1][p.col + 1].color != piece.color)
+            moves.add(new Point(p.row - 1, p.col + 1));
+        if (p.row + 1 < 8 && p.col - 1 >= 0 && board[p.row + 1][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row + 1, p.col - 1));
+        if (p.row + 1 >= 0 && p.col + 1 >= 0 && board[p.row + 1][p.col + 1].color != piece.color)
+            moves.add(new Point(p.row + 1, p.col + 1));
+        if (p.row - 1 >= 0 && board[p.row - 1][p.col].color != piece.color)
+            moves.add(new Point(p.row - 1, p.col));
+        if (p.row + 1 < 8 && board[p.row + 1][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row + 1, p.col));
+        if (p.col - 1 >= 0 && board[p.row][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row, p.col - 1));
+        if (p.col + 1 < 8 && board[p.row - 1][p.col + 1].color != piece.color)
+            moves.add(new Point(p.row, p.col + 1));
         return moves;
     };
 
     public BiFunction<Point, Piece[][], List<Point>> knight = (p, board) -> {
         ArrayList<Point> moves = new ArrayList<Point>();
         Piece piece = board[p.row][p.col];
+        if (p.row - 2 >= 0 && p.col - 1 >= 0 && board[p.row - 2][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row - 2, p.col - 1));
+        if (p.row - 2 >= 0 && p.col + 1 < 8 && board[p.row - 2][p.col + 1].color != piece.color)
+            moves.add(new Point(p.row - 2, p.col + 1));
+        if (p.row - 1 >= 0 && p.col - 2 >= 0 && board[p.row - 1][p.col - 2].color != piece.color)
+            moves.add(new Point(p.row - 1, p.col - 2));
+        if (p.row - 1 >= 0 && p.col + 2 < 8 && board[p.row - 1][p.col + 2].color != piece.color)
+            moves.add(new Point(p.row - 1, p.col + 2));
+        if (p.row + 2 < 8 && p.col - 1 >= 0 && board[p.row + 2][p.col - 1].color != piece.color)
+            moves.add(new Point(p.row + 2, p.col - 1));
+        if (p.row + 2 < 8 && p.col + 1 < 8 && board[p.row + 2][p.col + 1].color != piece.color)
+            moves.add(new Point(p.row + 2, p.col + 1));
+        if (p.row + 1 < 8 && p.col - 2 >= 0 && board[p.row + 1][p.col - 2].color != piece.color)
+            moves.add(new Point(p.row + 1, p.col - 2));
+        if (p.row + 1 < 8 && p.col + 2 < 8 && board[p.row + 1][p.col + 2].color != piece.color)
+            moves.add(new Point(p.row + 1, p.col + 2));
         return moves;
     };
 
